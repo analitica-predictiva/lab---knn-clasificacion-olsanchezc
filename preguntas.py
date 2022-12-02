be2eab92616754077d5a4f84493d8b56f0edccc4
Oscar Leonel <olsanchezc@unal.edu.co>
2022-12-02 17:33:27 -0500

    Errores correjidos

--- preguntas.py
+++ preguntas.py
@@ -66,7 +66,7 @@ def pregunta_02():
     
 
     # Cree un un clasificador k-NN con 6 vecinos
-    knn = KNeighborsClassifier(n_neighbors=6)
+    knn = KNeighborsClassifier(n_neighbors=5)
 
     # Entrene el clasificador con el conjunto de entrenamiento
     knn.fit(X, y)
@@ -79,3 +79,4 @@ def pregunta_02():
 
     # Retorne la matriz de confusión
     return confusion_matrix(y, y_pred)
+
